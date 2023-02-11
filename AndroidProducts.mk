89334d3877aa0b88701bd10236cfe51f622616e1
electimon <electimon@gmail.com>
2023-02-11 12:52:45 +0800

    okinawa: Move targets to common targets directory
    Signed-off-by: electimon <electimon@gmail.com>

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -12,8 +12,4 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-PRODUCT_MAKEFILES := $(LOCAL_DIR)/aosp_okinawa.mk
-
-COMMON_LUNCH_CHOICES += \
-    aosp_okinawa-eng \
-    aosp_okinawa-userdebug
+PRODUCT_MAKEFILES := $(wildcard device/motorola/targets/devices/okinawa/*.mk)
